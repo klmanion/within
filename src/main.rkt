abace04891a3dfe0090c6793aa54adc9a4c4bffd
Kurt L. Manion <klmanion@gmail.com>
2018-06-27 20:24:52 -0400

    added the code to display canvas

--- src/main.rkt
+++ src/main.rkt
@@ -1,8 +1,18 @@
 #lang racket/base
 (require racket/class
   racket/gui/base)
+(require "game-canvas.rkt")
 
 ;; Within
 ;
+
+(define f (new frame%
+               [label "Within"]
+               [style '(fullscreen-button)]))
+
+(define cv (new game-canvas% [parent f]))
+
+(module+ main
+  (send f show #t))
 
 ; vim: set ts=2 sw=2 expandtab lisp tw=79:
