b28c8de41b94311c868a2f4b9580336731b2ec6e
Kurt L. Manion <klmanion@gmail.com>
2018-06-27 20:08:53 -0400

    inital canvas commit

---
+++ src/game-canvas.rkt
@@ -0,0 +1,30 @@
+#lang racket/base
+(require racket/class
+  racket/gui/base)
+(require "parasite.rkt")
+
+(define game-canvas%
+  (class canvas%
+    (super-new [min-width 485]
+               [min-height 300])
+    (inherit get-dc refresh)
+    (field [player (new parasite%)])
+
+    (define/override on-paint
+      (λ ()
+        (let ([dc (get-dc)])
+          (send dc set-background (make-object color% #x0 #x0 #x0))
+          (send dc set-smoothing 'unsmoothed)
+          (send dc clear)
+
+          (unless (eq? player #f)
+            (send player draw dc)))))
+
+    (define/override on-size
+      (λ (width height)
+        (let ([dc (get-dc)]
+              [scale (min (/ width 485)
+                          (/ height 300))])
+          (send dc set-scale scale scale))))
+))
+
