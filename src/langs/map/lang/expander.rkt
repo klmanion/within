217ba589e1e59375b72eda74141d499853d296a2
Kurt L. Manion <klmanion@gmail.com>
2018-07-14 19:41:08 -0400

    removed deprecated helper functions

--- src/langs/map/lang/expander.rkt
+++ src/langs/map/lang/expander.rkt
@@ -2,9 +2,9 @@
 
 (require (for-syntax racket/base
            racket/class
-           syntax/parse
-           racket/stxparam))
-(require syntax/parse)
+           syntax/parse))
+(require syntax/parse
+  racket/stxparam)
 (module+ test
   (require rackunit rackunit/text-ui))
 
@@ -106,38 +106,6 @@
 
 (define-syntax-parameter current-obj #f)
 (define-syntax-parameter current-container #f)
-
-;; Helper functions {{{
-;
-
-(define-for-syntax clause-head->id
-  (syntax-rules ()
-    [(_ nm id) id]))
-
-(define-for-syntax room-clause->define
-  (λ (rc-stx . bss)
-    (cond
-      [(null? rc-stx) (void)]
-      [else
-       (syntax-parse rc-stx
-         [(_ rch rcb ...)
-          #'((begin
-               (define (clause-head->id rch) (new room%))
-               #,(unless (null? bss)
-                   (room-clause->define (car bss) (cdr bss)))))])])))
-
-(define-syntax id-mutator
-  (syntax-rules ()
-    [(id)
-     (datum->syntax (string-append "set-" (syntax->datum id) "!"))]))
-
-(define-syntax clause-header->class
-  (syntax-rules ()
-    [(_ cn _)
-     (datum->syntax
-       (string-append (string-downcase (syntax-e cn))
-                      "%"))]))
-;; }}}
 
 (define-syntax program
   (syntax-parser
