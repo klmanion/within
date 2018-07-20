42c17e60c3ecffdaf4c48b890a643c8e0b97e00b
Kurt L. Manion <klmanion@gmail.com>
2018-07-20 19:01:27 -0400

    added set-pos! skelleton to door%

--- src/door.rkt
+++ src/door.rkt
@@ -14,8 +14,12 @@
 (define door%
   (class* entity% (door<%>)
     (super-new)
-    (init-field [dest #f])
+    (init-field [dest #f] [pos #f])
 
+    (define/public set-pos!
+      (λ (npos)
+        ;; TODO add code that sets the x,y of door relative to room size
+        (set! pos npos)))
 ))
 
 ; vim: set ts=2 sw=2 expandtab lisp tw=79:
