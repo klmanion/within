f0762eebeb6dc97fc2ed47c9e564a61cc730a3d6
Kurt L. Manion <klmanion@gmail.com>
2018-07-20 21:38:50 -0400

    added info docs

---
+++ src/info.rkt
@@ -0,0 +1,14 @@
+#lang info
+
+(define collection "within")
+
+(define pkg-desc
+  (string-append
+     "As an alien organism take control of a ship by "
+     "infecting its crew and occupying sub-systems"))
+
+(define pkg-authors '(Kurt L. Manion))
+
+(define version "v0.2.1")
+
+; vim: set ts=2 sw=2 expandtab lisp tw=79:
